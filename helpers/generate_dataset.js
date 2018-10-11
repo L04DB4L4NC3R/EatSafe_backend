8b108e988ba30c8626bb53db45af18500c29b3b4
angad <angadsharma1016@gmail.com>
2018-10-12 00:45:34 +0530

    dataset

--- helpers/generate_dataset.js
+++ helpers/generate_dataset.js
@@ -1,11 +1,11 @@
 const fs = require("fs")
 let arr = [];
-for(let i=0;i<50;i++){
+for(let i=0;i<100;i++){
     for(let j=35;j<95;j++){
             
                  
                     
-                        if( (i>48 || i< 18) || j<20){
+                        if( (i>50 || i< 18) || j<20){
                             arr.push({
                                 input:[i,j,125,850,-7],
                                 output:[false]
